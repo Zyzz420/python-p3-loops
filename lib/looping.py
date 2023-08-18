d019ef8c7728c23271e6ac64c43726c3000c0794
Zyzz king <abdulaziz.komara@student.moringaschool.com>
2023-08-18 01:48:00 -0400

    Assignment complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    else:
+        print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    result = []
+    for num in int_list:
+        int_value = num ** 2
+        result.append(int_value)
+    return result
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
